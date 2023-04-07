2ce2142e29fed1460d0dd6e0aa91368dfe56a7b9
djabirpc <djabir.pc@outlook.fr>
2023-04-07 10:28:43 +0100

    Basics 30

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -12,6 +12,13 @@ export class ServersComponent implements OnInit{
   allowNewServer = false;
   serverCreationStatus = "No sercer was created!";
   serverName = 'Test Server';
+  servers = ['TestSece','Test 32']
+  serverCreated = false;
+
+  username:string = ''
+
+  showSecret = false;
+  log = [];
 
   constructor(){
     setTimeout(()=> {
@@ -24,10 +31,19 @@ export class ServersComponent implements OnInit{
   }
 
   onCreateServer(){
-    this.serverCreationStatus = 'Server Was Created'
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus = 'Server Was Created Name is ' + this.serverName;
   }
 
-  onUpdateServer(event: Event){
-    this.serverName = (<HTMLInputElement>event.target).value;
+  // onUpdateServer(event: Event){
+  //   this.serverName = (<HTMLInputElement>event.target).value;
+  // }
+
+  onToggleDetails(){
+    this.showSecret = !this.showSecret;
+    // this.log.push(this.log.length + 1);
+    this.log.push(new Date())
   }
+
 }
