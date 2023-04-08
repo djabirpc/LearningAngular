b1e188ba28b8be3ae3ba9ee1ee3d7d0b7b18f9a3
djabirpc <djabir.pc@outlook.fr>
2023-04-08 16:00:57 +0100

    Components And Databinding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // styleUrls: ['./app.component.css']
-  styles:[`
-    h1{
-      color: green;
-    }
-  `]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name = 'my app';
+  leadedFeature = 'recipe';
+  onNavigate(feature: string){
+    this.leadedFeature = feature;
+  }
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,6 +7,8 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  @Output() recipeWasSelected= new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
     new Recipe(
       'A Test Recipe',
@@ -14,9 +16,15 @@ export class RecipeListComponent {
       'https://www.indianhealthyrecipes.com/wp-content/uploads/2021/10/mint-chutney-pudina-chutney-recipe.jpg'
       ),
     new Recipe(
-      'A Test Recipe',
+      'Another Test Recipe',
       'A Desc Test',
       'https://www.indianhealthyrecipes.com/wp-content/uploads/2021/10/mint-chutney-pudina-chutney-recipe.jpg'
       )
   ];
+
+
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe)
+  }
+
 }
