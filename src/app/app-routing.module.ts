98307edeb7490545088c50f8fbd14683acb97fbd
djabirpc <djabir.pc@outlook.fr>
2023-04-14 11:29:43 +0100

    Routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from "./routing/home/home.component";
+import { UsersComponent } from "./routing/users/users.component";
+import { UserComponent } from "./routing/users/user/user.component";
+import { ServersComponent } from "./routing/servers/servers.component";
+import { ServerComponent } from "./routing/servers/server/server.component";
+import { EditServerComponent } from "./routing/servers/edit-server/edit-server.component";
+import { PageNotFoundComponent } from "./routing/page-not-found/page-not-found.component";
+import { AuthGuard } from "./auth-guard.service";
+import { CanDeactivateGuard } from "./routing/servers/edit-server/can-deactivate-guard.service";
+import { ErrorPageComponent } from "./routing/error-page/error-page.component";
+import { ServerResolver } from "./routing/servers/server/server-resolver.service";
+
+const appRoutes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'users', component: UsersComponent, children: [
+      { path: ':id/:name', component: UserComponent },
+    ] },
+    { 
+        path: 'servers', 
+        // canActivate:[AuthGuard] ,
+        canActivateChild: [AuthGuard],
+        component: ServersComponent, 
+        children: [
+      { path: ':id', component: ServerComponent, resolve: {server: ServerResolver} },
+      { path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard] },
+    ] }, 
+    // { path: 'not-found', component: PageNotFoundComponent },
+    { path: 'not-found', component: ErrorPageComponent, data: {message: 'Page not found'} },
+    { path : '**', redirectTo: '/not-found'},
+  ];
+
+@NgModule({
+    imports: [
+        // RouterModule.forRoot(appRoutes, {useHash: true})
+        RouterModule.forRoot(appRoutes)
+    ],
+    exports: [RouterModule]
+})
+export class AppRoutingModule{
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,24 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shooping-list.service';
+import { RoutingComponent } from './routing/routing.component';
+import { HomeComponent } from './routing/home/home.component';
+import { ServersComponent } from './routing/servers/servers.component';
+import { UsersComponent } from './routing/users/users.component';
+import { EditServerComponent } from './routing/servers/edit-server/edit-server.component';
+import { ServerComponent } from './routing/servers/server/server.component';
+import { UserComponent } from './routing/users/user/user.component';
+import { ServersService } from './routing/servers/servers.service';
+import { PageNotFoundComponent } from './routing/page-not-found/page-not-found.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AuthService } from './auth.service';
+import { AuthGuard } from './auth-guard.service';
+import { CanDeactivateGuard } from './routing/servers/edit-server/can-deactivate-guard.service';
+import { ErrorPageComponent } from './routing/error-page/error-page.component';
+import { ServerResolver } from './routing/servers/server/server-resolver.service';
+
+
+
 
 @NgModule({
   declarations: [
@@ -25,13 +43,30 @@ import { ShoppingListService } from './shopping-list/shooping-list.service';
     ShoppingListComponent,
     ShoppingEditComponent,
     DropdownDirective,
+    RoutingComponent,
+    HomeComponent,
+    ServersComponent,
+    ServerComponent,
+    EditServerComponent,
+    UsersComponent,
+    UserComponent,
+    PageNotFoundComponent,
+    ErrorPageComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    AppRoutingModule
   ],
-  providers: [ShoppingListService],
+  providers: [
+    ShoppingListService,
+    ServersService,
+    AuthService,
+    AuthGuard,
+    CanDeactivateGuard,
+    ServerResolver
+  ],
   bootstrap: [AppComponent]
 }) 
 export class AppModule { }
