457f632228260ab4c01bf4630b6ccf710f8d4ba2
djabirpc <djabir.pc@outlook.fr>
2023-04-26 15:02:08 +0100

    forms 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { ShoppingListService } from './shopping-list/shooping-list.service';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
+import { FormsComponent } from './forms/forms.component';
 
 @NgModule({
   declarations: [
@@ -30,6 +31,7 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
     DropdownDirective,
     RecipeStartComponent,
     RecipeEditComponent,
+    FormsComponent,
   ],
   imports: [
     BrowserModule,
